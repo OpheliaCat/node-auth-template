fe19af592309326bdedf96e08572e85c5a9d0e05
Pavlov Oleh <opavlov@conceptsoft.io>
2020-04-14 04:42:33 +0900

    changed auth service

--- back/services/auth.service.js
+++ back/services/auth.service.js
@@ -1,27 +1,40 @@
 const crypto = require('crypto');
 
 module.exports = {
-    genCredentialsFlow: function* (password) {
-        const salt = crypto.randomBytes(32).toString('hex');
-        const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'hex');
-        const id = yield { salt, hash };
-        const b64 = json => new Buffer(JSON.stringify(json)).toString('base64');
-        const header = b64({ alg: "HS256", type: "jwt" });
-        const payload = b64({ exp: Date.now() + 3600, id });
-        const signature = crypto
-            .createHmac('sha256', process.env.JWT_SECRET)
-            .update(`${header}.${payload}`)
-            .digest('base64');
-        yield `${header}.${payload}.${signature}`
+    encryptPassword: password => {
+        const salt = crypto
+            .randomBytes(32)
+            .toString('hex');
+        const hash = crypto
+            .pbkdf2Sync(password, salt, 1000, 64, 'hex')
+            .toString('hex');
+        return `${hash}.${salt}`
     },
 
-    authorizationFlow: function* (authorizationHeader) {
-        const { header, payload, signature } = 
-            authorizationHeader.replace('Bearer', '').trim().split('.');
-        const testSignature = crypto
-            .createHmac('sha256', process.env.JWT_SECRET)
-            .update(`${header}.${payload}`)
-            .digest('base64');
-        yield signature === testSignature ? new Buffer(payload, 'base64').toString('ascii') : null;
+    comparePassword: (password, passwordHash) => {
+        const [hash, salt] = passwordHash.split('.');
+        const testHash = crypto
+            .pbkdf2Sync(password, salt, 1000, 64, 'hex')
+            .toString('hex');
+        return hash === testHash
+    },
+
+    signJWT: identity => {
+        const b64 = json => new Buffer(JSON.stringify(json)).toString('base64');
+        const header = b64({ alg: "HS256", type: "jwt" });
+        const payload = b64({ exp: Date.now() + 3600, identity });
+        const signature = crypto
+            .sign('sha256', `${header}.${payload}`, process.env.JWT_SECRET)
+            .toString('base64');
+        return `${header}.${payload}.${signature}`
+    },
+    
+    verifyJWT: token => {
+        const { header, payload, signature } = token
+            .replace('Bearer', '')
+            .trim()
+            .split('.');
+        return crypto
+            .verify('sha256', `${header}.${payload}`, process.env.JWT_SECRET, signature)
     }
 }
